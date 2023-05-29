7a6523024dd3a5141602d35536bc5e766b4deece
Onur Yueksek <yueksek.onur@gmail.com>
2023-05-29 14:24:14 +0200

    Solve Kata: Remove exclamation marks

---
+++ RemoveExclamationMarks.js
@@ -0,0 +1,15 @@
+/**
+ * Description:
+ *
+ * Write function RemoveExclamationMarks which removes
+ * all exclamation marks from a given string.
+ *
+ * Kata URL: https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+ *
+ * @param {*} s
+ * @returns
+ */
+
+function removeExclamationMarks(s) {
+  return s.replace(/!/g, "");
+}
