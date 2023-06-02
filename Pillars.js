cbacb6fb34c095587c10c124fafa792dafeb2ec0
Onur Yueksek <yueksek.onur@gmail.com>
2023-06-02 23:43:18 +0200

    Solve Kata: Pillars

---
+++ Pillars.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * There are pillars near the road. The distance between
+ * the pillars is the same and the width of the pillars
+ * is the same. Your function accepts three arguments:
+ *
+ * number of pillars (≥ 1);
+ *
+ *  distance between pillars (10 - 30 meters);
+ *  width of the pillar (10 - 50 centimeters).
+ *
+ * Calculate the distance between the first and the
+ * last pillar in centimeters
+ * (without the width of the first and last pillar).
+ *
+ *  Kata URL: https://www.codewars.com/kata/5bb0c58f484fcd170700063d
+ *
+ *
+ * @param {*} numPill
+ * @param {*} dist
+ * @param {*} width
+ */
+
+function pillars(numPill, dist, width) {
+  return numPill >= 2
+    ? (numPill - 1) * (dist * 100) + width * (numPill - 2)
+    : 0;
+}
