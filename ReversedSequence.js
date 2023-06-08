a8315dc7a618ef33b606af36bfec2a439c6d6193
Onur Yueksek <yueksek.onur@gmail.com>
2023-06-08 15:40:28 +0200

    Solve: Reversed sequence

---
+++ ReversedSequence.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * Description:
+ *
+ * Build a function that returns an array of
+ * integers from n to 1 where n>0.
+ *
+ * Example : n=5 --> [5,4,3,2,1]
+ * Fundamentals
+ *
+ *
+ * @param {*} n
+ * @returns
+ */
+const reverseSeq = (n) => {
+  const result = [];
+  for (let i = n; i != 0; i--) {
+    result.push(i);
+  }
+  return result;
+};
